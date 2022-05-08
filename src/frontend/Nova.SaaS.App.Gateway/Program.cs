41d60bfd9c7371ee60cb94914b75446a4f830c7a
Admir Durmishi <durmisi@gmail.com>
2022-05-08 21:41:45 +0200

    Application configuration improvements

---
+++ src/frontend/Nova.SaaS.App.Gateway/Program.cs
@@ -0,0 +1,46 @@
+using Nacos.V2.DependencyInjection;
+using Yarp.ReverseProxy.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddNacosV2Naming(x =>
+{
+    x.ServerAddresses = new System.Collections.Generic.List<string> {
+        "http://nacos:8848/"
+    };
+
+    x.EndPoint = "";
+
+    x.Namespace = "yarp";
+
+    x.UserName = "nacos";
+
+    x.Password = "nacos";
+
+    // swich to use http or rpc
+    x.NamingUseRpc = true;
+});
+
+builder.Services.AddReverseProxy()
+    .AddNacosServiceDiscovery(
+    groupNames: "DEFAULT_GROUP",
+    percount: 100,
+    enableAutoRefreshService: true,
+    autoRefreshPeriod: 30);
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/yarp", (IProxyConfigProvider provider) =>
+    {
+        var res = provider.GetConfig();
+        return Results.Ok(res);
+    });
+
+    endpoints.MapReverseProxy();
+});
+
+app.Run();
