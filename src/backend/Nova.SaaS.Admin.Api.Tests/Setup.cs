1d54690e58d796e3a8281317bbee825f8f05986e
Admir Durmishi <durmisi@gmail.com>
2022-05-22 10:18:54 +0200

    Add XUnit logger provider

--- src/backend/Nova.SaaS.Admin.Api.Tests/Setup.cs
+++ src/backend/Nova.SaaS.Admin.Api.Tests/Setup.cs
@@ -1,6 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using MartinCostello.Logging.XUnit;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Nova.SaaS.Admin.Api.Data;
+using Nova.SaaS.Admin.Api.Services;
+using System;
 using System.Net.Http;
+using Xunit.Abstractions;
 
 namespace Nova.SaaS.Admin.Api.Tests
 {
@@ -8,18 +17,68 @@ namespace Nova.SaaS.Admin.Api.Tests
     {
         private static readonly WebApplicationFactory<Program> factory = new();
 
-        public static HttpClient ApiClient { get; } = factory
+        public static HttpClient GetClient(ITestOutputHelper outputHelper) => factory
             .WithWebHostBuilder(b =>
-                b.ConfigureTestServices(s =>
-                {
+            {
+                b.ConfigureServices(services =>
+                        {
+                            services.RemoveAll<ILoggerProvider>();
+                            services.RemoveAll<ILoggerFactory>();
+                            services.RemoveAll<ILogger>();
+                            services.AddLogging(builder =>
+                            {
+                                builder.ClearProviders();
 
-                })
-            ).CreateClient();
+                                builder
+                                .AddConsole()
+                                .AddDebug()
+                                .AddProvider(new XUnitLoggerProvider(outputHelper, new XUnitLoggerOptions()
+                                {
+                                    IncludeScopes = true,
+                                }));
+                            });
 
-       
+                            
+                            services.RemoveAll<IDataContextService>();
+                            services.TryAddTransient<IDataContextService, TestDataContextService>();
+                        });
+
+            }).CreateClient();
+
+
+        public static NovaSaasAdminDbContext CreateContext()
+        {
+            var service = (IDataContextService)factory.Services.GetService(typeof(IDataContextService));
+            return service.CreateContext();
+        }
+
         static Setup()
         {
-            
+
         }
     }
+
+
+    public class TestDataContextService : IDataContextService
+    {
+        private readonly ILoggerFactory loggerFactory;
+
+        public TestDataContextService(ILoggerFactory loggerFactory)
+        {
+            this.loggerFactory = loggerFactory;
+        }
+        public NovaSaasAdminDbContext CreateContext()
+        {
+            var databaseName = "NovaSaasAdminDb";
+
+            var options = new DbContextOptionsBuilder<NovaSaasAdminDbContext>()
+                .UseInMemoryDatabase(databaseName)
+                .EnableSensitiveDataLogging()
+                .UseLoggerFactory(loggerFactory)
+                .Options;
+
+            return new NovaSaasAdminDbContext(options);
+        }
+    }
+
 }
--- src/backend/Nova.SaaS.Admin.Api.Tests/ValuesEndpointsTests.cs
+++ src/backend/Nova.SaaS.Admin.Api.Tests/ValuesEndpointsTests.cs
@@ -1,24 +1,46 @@
+using System.Linq;
 using System.Threading.Tasks;
 
 using FastEndpoints;
 
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Nova.SaaS.Admin.Api.Tests
 {
 
     public class ValuesEndpointsTests
     {
+        private readonly ITestOutputHelper outputHelper;
+
+        public ValuesEndpointsTests(ITestOutputHelper outputHelper)
+        {
+            this.outputHelper = outputHelper;
+        }
+
         [Fact]
         public async Task GetValuesEndpointReturnsOK()
         {
-            var (_, res) = await Setup.ApiClient.GETAsync<
+            //using (var context = Setup.CreateContext())
+            //{
+            //    context.Values.Add(new Data.Models.Value()
+            //    {
+            //        Id = System.Guid.NewGuid(),
+            //        Name = "Test Value",
+            //        Created = System.DateTime.Now
+            //    });
+
+            //    context.SaveChanges();
+            //}
+
+            var (_, res) = await Setup.GetClient(outputHelper).GETAsync<
                 Endpoints.Values.GetValues,
                 Endpoints.Values.GetValues.GetValuesResponse>();
 
             Assert.NotNull(res);
             Assert.NotEmpty(res?.RequestInfo);
             Assert.NotEmpty(res?.Values);
+            Assert.Equal(1, res?.Values.Count());
         }
     }
 }
